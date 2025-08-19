c4f30ee6a71a0d79a3817c11046c58a4a356c926
Muhammad Rafky <muhammad.rafky@amartha.com>
2025-08-19 15:33:08 +0700

    feat: remove arch checking

--- build.zig
+++ build.zig
@@ -1,23 +1,6 @@
 const std = @import("std");
-const builtin = @import("builtin");
 
 pub fn build(b: *std.Build) void {
-
-    // If CPU arch is not `x86_64` or not `x86`
-    if (builtin.cpu.arch != .x86_64 and builtin.cpu.arch != .x86) {
-        @compileError("MinHook can be compiled only for `x86_64` and `x86` CPU arch");
-    }
-
-    // If target OS is not `Windows`
-    if (builtin.os.tag != .windows) {
-        @compileError("MinHook can be compiled only for Windows");
-    }
-
-    // If target ABI is not `GNU`
-    if (builtin.abi != .gnu) {
-        @compileError("MinHook can be compiled only for GNU ABI");
-    }
-
     const target = b.standardTargetOptions(.{});
     const optimize = b.standardOptimizeOption(.{});
 
